856fba4df59543f8c7837259a4cc3ab0eae12121
Muneebakhan <muneebakhan430@gmail.com>
2022-07-23 01:41:53 +0500

    Updated work 4

--- AddToCard/Controllers/HomeController.cs
+++ AddToCard/Controllers/HomeController.cs
@@ -30,17 +30,53 @@ namespace AddToCard.Controllers
         {
             tbl_Product p = db.tbl_Product.Where(x => x.ProdId == id).SingleOrDefault();
             Cart c = new Cart();
-            c.prodid = id;
-            c.proname = p.ProductName;
-            c.Image = p.Image;
-            c.price = Convert.ToInt32(p.Price);
-            c.qty = Convert.ToInt32(qty);
-            c.bill = c.qty * c.price;
+            c.prodid = id; //4
+            c.proname = p.ProductName; //Bag
+            c.Image = p.Image;//img6
+            c.price = Convert.ToInt32(p.Price); //1000
+            c.qty = Convert.ToInt32(qty);//2
+            c.bill = c.qty * c.price; //2 * 1000 =2000
 
             if(Session["Cart"] == null)
             {
                 li.Add(c);
                 Session["Cart"] = li;
+            }
+            else
+            {
+                
+                List<Cart> li2 =  Session["Cart"] as List<Cart>;
+                int flag = 0;
+
+                foreach(var item in li2)
+                {
+                    if(item.prodid == c.prodid)
+                    {
+                        item.bill += c.bill;
+                        item.qty += c.qty;
+                        flag = 1;
+
+                    }
+                    
+                }
+           
+                if(flag == 0)
+                {
+                    li2.Add(c);
+                }
+                Session["Cart"] = li2;
+            }
+
+            if (Session["Cart"] != null)
+            {
+                int x = 0;
+                List<Cart> li2 = Session["Cart"] as List<Cart>;
+                foreach (var item in li2)
+                {
+                    x += item.bill;
+                }
+                Session["total"] = x;
+                Session["item_count"] = li2.Count();
             }
             return RedirectToAction("Index");
         }
@@ -49,6 +85,29 @@ namespace AddToCard.Controllers
         public ActionResult Checkout()
         {
             return View();
+        }
+
+        public ActionResult remove(int id)
+        {
+            if(Session["Cart"] == null)
+            {
+                Session.Remove("total");
+                Session.Remove("Cart");
+            }
+            else
+            {
+                List<Cart> li2 = Session["Cart"] as List<Cart>;
+                Cart c = li2.Where(x => x.prodid == id).SingleOrDefault();
+                li2.Remove(c);
+                int s = 0;
+                foreach(var item in li2)
+                {
+                    s += item.bill;
+                }
+                Session["total"] = s;
+                Session["item_count"] = li2.Count();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
